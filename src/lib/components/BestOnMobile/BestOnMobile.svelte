c2ed64a34b446bf413bc25bb2812cd4723848a5d
Bash Forgo <6547523+bashforgo@users.noreply.github.com>
2023-09-20 23:24:05 +0100

    add best on mobile note

---
+++ src/lib/components/BestOnMobile/BestOnMobile.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+
+	let isVisible = false;
+
+	const show = () => {
+		isVisible = true;
+		setTimeout(() => {
+			isVisible = false;
+		}, 3000);
+	};
+
+	onMount(() => {
+		const { matches } = window.matchMedia('(hover: none) and (max-width: 800px)');
+		if (!matches) show();
+	});
+</script>
+
+{#if isVisible}
+	<footer transition:fly={{ y: 100 }}>📱 This page is best viewed on a mobile device.</footer>
+{/if}
+
+<style>
+	footer {
+		position: fixed;
+		bottom: 16px;
+		left: 50%;
+		transform: translateX(-50%);
+		background: #222;
+		padding: 16px;
+		border-radius: 16px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import BestOnMobile from '$lib/components/BestOnMobile/BestOnMobile.svelte';
 	import '$lib/global.css';
 </script>
 
 <slot />
+
+<BestOnMobile />
